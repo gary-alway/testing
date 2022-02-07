31ac7f6d3334ee695540b01f47854e45b9afb201
Gary Alway <garydouglasalway@gmail.com>
2022-02-07 22:20:03 +0000

    put api test

--- src/api/beersService.test.ts
+++ src/api/beersService.test.ts
@@ -1,6 +1,6 @@
 import nock, { cleanAll, isDone } from 'nock'
 import { testBeer } from '../../test/testFactories'
-import { BEER_BASE_URL, getBeers } from './beersService'
+import { BASE_URL, getBeers, putBeer } from './beersService'
 
 const beer1 = testBeer({ name: 'beer1' })
 const beer2 = testBeer({ name: 'beer2' })
@@ -12,7 +12,7 @@ describe('beers service', () => {
 
   describe('getBeers', () => {
     it('returns the full list of beers', async () => {
-      nock(BEER_BASE_URL).get('/beers').reply(200, beers)
+      nock(BASE_URL).get('/beers').reply(200, beers)
 
       const result = await getBeers()
 
@@ -21,11 +21,21 @@ describe('beers service', () => {
     })
 
     it('returns a list of beers filtered by beer name', async () => {
-      nock(BEER_BASE_URL).get('/beers?beer_name=lager').reply(200, beers)
+      nock(BASE_URL).get('/beers?beer_name=lager').reply(200, beers)
 
       const result = await getBeers('lager')
 
       expect(result).toEqual(beers)
+      expect(isDone()).toBe(true)
+    })
+  })
+
+  describe('putBeer', () => {
+    it('uploads a new beer', async () => {
+      nock(BASE_URL).put('/beers', { data: beer1 }).reply(200)
+
+      await putBeer(beer1)
+
       expect(isDone()).toBe(true)
     })
   })
--- src/api/beersService.ts
+++ src/api/beersService.ts
@@ -1,8 +1,13 @@
 import request from 'superagent'
 
-export const BEER_BASE_URL = 'https://api.punkapi.com/v2'
+export const BASE_URL = 'https://api.punkapi.com/v2'
 
 export const getBeers = async (name?: string): Promise<Beer[]> => {
-  const url = name ? `${BEER_BASE_URL}/beers?beer_name=${name}` : `${BEER_BASE_URL}/beers`
+  const url = name ? `${BASE_URL}/beers?beer_name=${name}` : `${BASE_URL}/beers`
   return request.get(url).then(({ body }) => body as Beer[])
+}
+
+export const putBeer = async (beer: Beer): Promise<void> => {
+  const url = `${BASE_URL}/beers`
+  await request.put(url).send({ data: beer })
 }
